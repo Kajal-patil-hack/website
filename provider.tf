a1ff1771f1d781591f71c3942d58ac59a77f6d82
Kajal-patil-hack <kajalpatil199902@gmail.com>
2024-12-30 18:39:45 +0530

    hello

--- provider.tf
+++
@@ -1,8 +0,0 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "ap-south-1"
-  /*
-  access_key = "AKIA4VDBMHQ7RJNUS6YF"
-  secret_key = "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"
-  */
-}
