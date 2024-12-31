ad8a46b9d24daf66d9ff95dd381dbbb6e5e80fbb
Kajal-patil-hack <kajalpatil199902@gmail.com>
2024-12-31 08:56:57 +0530

    Hello

---
+++ instance.tf
@@ -0,0 +1,33 @@
+# Creating key-pair on AWS using SSH-public key
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key-name
+  public_key = file("${path.module}/my-key.pub")
+}
+
+# Creating a security group to restrict/allow inbound connectivity
+resource "aws_security_group" "network-security-group" {
+  name        = var.network-security-group-name
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  # Not recommended to add "0.0.0.0/0" instead we need to be more specific with the IP ranges to allow connectivity from.
+  tags = {
+    Name = "nsg-inbound"
+  }
+}
+
+
+# Creating Ubuntu EC2 instance
+resource "aws_instance" "example_server" {
+  ami             = "ami-0fd05997b4df4dff7aac"
+  instance_type   = "t2.micro"
+  tags = {
+    Name = "web1"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,8 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+  /*
+  access_key = "AKIA4VDBMHQ7RJNUS6YF"
+  secret_key = "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"
+  */
+}
